009c49a64d86ed50d801acc8a3945711c3942e63
KenjiKamimoto-wustl122 <kamimoto@wustl.edu>
2020-07-20 16:36:50 -0500

    add function for data integration

--- celloracle/utility/utility.py
+++ celloracle/utility/utility.py
@@ -15,7 +15,9 @@ from tqdm.notebook import tqdm
 
 from sklearn.preprocessing import StandardScaler
 
-
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
+from sklearn.decomposition import PCA
+from sklearn.metrics import r2_score
 
 def save_as_pickled_object(obj, filepath):
     """
@@ -190,6 +192,20 @@ def standard(df):
 
     return(dt)
 
+## Anndata processing functions
+
+def update_adata(adata):
+    # Update Anndata
+    # Anndata generated with Scanpy 1.4 or less should be updated with this function
+    # This function will be depricated in the future.
+
+    try:
+        lo = adata.uns['draw_graph']['params']['layout']
+        if isinstance(lo, np.ndarray):
+            lo = lo[0]
+        adata.uns['draw_graph']['params']['layout'] = lo
+    except:
+        pass
 
 def adata_to_color_dict(adata, cluster_use):
     """
@@ -210,31 +226,52 @@ def adata_to_color_dict(adata, cluster_use):
 
 
 
-def transfer_all_colors_between_anndata(anndata_ref, anndata_que):
+def transfer_all_colors_between_anndata(adata_ref, adata_que):
+    """
+    Extract all color information from reference anndata and transfer the color into query anndata.
 
-    # Get all clusters that have colors in anndata_ref
-    keys = list(anndata_ref.uns.keys())
+    Args:
+        adata_ref (anndata): reference anndata
+
+        adata_que (anndata): query anndata
+
+    """
+
+    # Get all clusters that have colors in adata_ref
+    keys = list(adata_ref.uns.keys())
     keys = [i.replace("_colors", "") for i in keys if "_colors" in i]
 
-    keys = [i for i in keys if i in anndata_que.obs.columns]
+    keys = [i for i in keys if i in adata_que.obs.columns]
 
     for cluster_name in keys:
-        transfer_color_between_anndata(anndata_ref=anndata_ref,
-                                      anndata_que=anndata_que,
+        transfer_color_between_anndata(adata_ref=adata_ref,
+                                      adata_que=adata_que,
                                       cluster_name=cluster_name)
 
     print("Color meta data were transfered for \n")
     print(" ", keys)
 
 
-def transfer_color_between_anndata(anndata_ref, anndata_que, cluster_name):
+def transfer_color_between_anndata(adata_ref, adata_que, cluster_name):
+    """
+    Extract color information from reference anndata and transfer the color into query anndata.
+
+    Args:
+        adata_ref (anndata): reference anndata
+
+        adata_que (anndata): query anndata
+
+        cluster_name (str): cluster name. This information should exist in the anndata.obs.
+
+    """
 
     # Get color as a dictionary
-    dic_ref = adata_to_color_dict(anndata_ref, cluster_name)
+    dic_ref = adata_to_color_dict(adata_ref, cluster_name)
 
 
     # Get color keys from que data
-    color_keys = list(anndata_que.obs[cluster_name].cat.categories)
+    adata_que.obs[i] = adata_que.obs[cluster_name].astype("category")
+    color_keys = list(adata_que.obs[cluster_name].cat.categories)
 
     # Convert color list
     colors_after = []
@@ -245,4 +282,86 @@ def transfer_color_between_anndata(anndata_ref, anndata_que, cluster_name):
     colors_after = np.array(colors_after)
 
     # Update color
-    anndata_que.uns[cluster_name + "_colors"] = colors_after
+    adata_que.uns[cluster_name + "_colors"] = colors_after
+
+
+def knn_data_transformer(adata_ref, adata_que, n_neighbors=20, cluster_name=None, embedding_name=None, adata_true=None, transfer_color=True, n_PCA=30):
+    """
+    Extract categorical information from adata.obs or embedding information from adata.obsm and transfer these information into query anndata.
+    In the calculation, KNN is used after PCA.
+
+    Args:
+        adata_ref (anndata): reference anndata
+
+        adata_que (anndata): query anndata
+
+        cluster_name (str or list of str): cluster name(s) to be transfered. If you want to transfer multiple data, you can set the cluster names as a list.
+
+        embedding_name (str or list of str): embedding name(s) to be transfered. If you want to transfer multiple data, you can set the embedding names as a list.
+
+        adata_true (str): This argument can be used for the validataion of this algorithm. If you have true answer, you can set it. If you set true answer, the function will return some metrics for benchmarking.
+
+        transfer_color (bool): Whether or not to transfer color data in addition to cluster information.
+
+        n_PCA (int): Number of PCs that will be used for the input of KNN algorithm.
+
+    """
+    # 1. Prepare data
+    X_train = adata_ref.to_df()
+    X_test = adata_que.to_df()
+
+    if ~np.all(X_train.columns == X_test.columns):
+        genes = np.intersect1d(X_train.columns, X_test.columns)
+        X_train = X_train[genes]
+        X_test = X_test[genes]
+
+    # 2. PCA
+    model_PCA = PCA(n_components=n_PCA)
+    X_train_PCA = model_PCA.fit_transform(X_train)
+    X_test_PCA = model_PCA.transform(X_test)
+
+    # 3. Learning and prediction
+    if cluster_name is not None:
+        if isinstance(cluster_name, str):
+            cluster_name = [cluster_name]
+        if isinstance(cluster_name, list):
+            for i in  cluster_name:
+                model_kncl = KNeighborsClassifier(n_neighbors=n_neighbors)
+                model_kncl.fit(X_train_PCA, adata_ref.obs[i])
+                pred = model_kncl.predict(X_test_PCA)
+                adata_que.obs[i] = pred
+                adata_que.obs[i] = adata_que.obs[i].astype("category")
+
+                if transfer_color:
+                    transfer_color_between_anndata(adata_ref=adata_ref, adata_que=adata_que, cluster_name=i)
+
+                if adata_true is not None:
+                    true = adata_true.obs[i]
+                    print(f"accuracy for {i}: ", np.mean(pred == true))
+        else:
+            raise ValueError("cluster name format error")
+
+    if embedding_name is not None:
+        if isinstance(embedding_name, str):
+            embedding_name = [embedding_name]
+        if isinstance(embedding_name, list):
+            for i in  embedding_name:
+                model_knreg = KNeighborsRegressor(n_neighbors=n_neighbors)
+                model_knreg.fit(X_train_PCA, adata_ref.obsm[i])
+                pred = model_knreg.predict(X_test_PCA)
+                adata_que.obsm[i] = pred
+
+                if adata_true is not None:
+                    true = adata_true.obsm[i]
+
+                    fig, ax = plt.subplots(1, 2)
+                    for k in [0, 1]:
+                        x = true[:, k]
+                        y = pred[:, k]
+                        ax[k].scatter(x, y)
+                        ax[k].set_xlabel("true")
+                        ax[k].set_ylabel("pred")
+                        ax[k].set_title(f"r: {r2_score(x, y): .3g}")
+                    plt.show()
+        else:
+            raise ValueError("embedding name format error")
