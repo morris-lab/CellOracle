233e1681863ef09ed994ab80f318d10f57dd4c91
KenjiKamimoto-wustl122 <kamimoto@wustl.edu>
2020-07-20 16:43:04 -0500

    add function for data integration

--- celloracle/utility/__init__.py
+++ celloracle/utility/__init__.py
@@ -12,7 +12,8 @@ from .utility import (save_as_pickled_object, load_pickled_object,
                       standard, inverse_dictionary,
                       adata_to_color_dict,
                       transfer_all_colors_between_anndata,
-                      transfer_color_between_anndata)
+                      transfer_color_between_anndata,
+                      knn_data_transferer)
 from .load_hdf5 import load_hdf5
 
 __all__ = [
@@ -26,5 +27,6 @@ __all__ = [
            "inverse_dictionary"
            "adata_to_color_dict",
            "transfer_all_colors_between_anndata",
-           "transfer_color_between_anndata"
+           "transfer_color_between_anndata",
+           "knn_data_transferer"
            ]
