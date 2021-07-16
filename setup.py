b1ed063d4001f570fbb358ddb0c2b3b8e923511c
KenjiKamimoto-wustl122 <kamimoto@wustl.edu>
2021-07-15 23:26:05 -0500

    version 0.7.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open('LICENSE') as f:
 
 setup(
     name='celloracle',
-    version="0.7.0",
+    version="0.7.1",
     description='GRN analysis with single cell data',
     long_description=readme,
     python_requires='>=3.6',
