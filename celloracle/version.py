59c7eb44623682c563247b2aeb673f74f1b3bb01
KenjiKamimoto-wustl122 <kamimoto@wustl.edu>
2021-07-27 21:22:46 -0500

    update TAIR10 base-GRN

--- celloracle/version.py
+++ celloracle/version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
 
-__version__ = '0.7.3'
+__version__ = '0.7.4'
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open('LICENSE') as f:
 
 setup(
     name='celloracle',
-    version="0.7.3",
+    version="0.7.4",
     description='GRN analysis with single cell data',
     long_description=readme,
     python_requires='>=3.6',
