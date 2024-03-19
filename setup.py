b828acade1b5d8371b720ec365043e453fbe78e3
KenjiKamimoto-wustl122 <kamimoto@wustl.edu>
2024-03-19 18:28:02 -0500

    modified:   celloracle/version.py modified:   setup.py

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
                 'Programming Language :: Python :: 3.6',
                 'Programming Language :: Python :: 3.7',
                 'Programming Language :: Python :: 3.8',
-                'Programming Language :: Python :: 3.10'
+                'Programming Language :: Python :: 3.10',
             ],
     install_requires=required,
     author=author,
