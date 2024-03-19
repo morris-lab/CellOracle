66d984912b47a232872d01876da5e9b8d950a367
KenjiKamimoto-wustl122 <kamimoto@wustl.edu>
2024-03-19 16:58:42 -0500

    support python 3.10

--- setup.py
+++ setup.py
@@ -53,7 +53,8 @@ setup(
                 # that you indicate whether you support Python 2, Python 3 or both.
                 'Programming Language :: Python :: 3.6',
                 'Programming Language :: Python :: 3.7',
-                'Programming Language :: Python :: 3.8'
+                'Programming Language :: Python :: 3.8',
+                'Programming Language :: Python :: 3.10'
             ],
     install_requires=required,
     author=author,
